4f8ba5e590df6890a87137cc2986ffbfe85cd847
jajalecapouet <nicolas.jarose@gmail.com>
2023-01-30 11:20:19 +0100

    pouet

--- front/react_project/src/main.tsx
+++ front/react_project/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Counter from './Counter'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Counter />
   </React.StrictMode>,
 )
