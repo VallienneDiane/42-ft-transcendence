a6cc2dbcca413548c46bf6731cf32629c945e298
Nicolas Jaros <njaros@z3r3p3.42lyon.fr>
2023-02-06 17:32:00 +0100

    boule a john

--- back/nest_project/src/app.controller.ts
+++ back/nest_project/src/app.controller.ts
@@ -10,3 +10,19 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+
+@Controller('johnboule')
+export class BoolJohnController {
+  @Get('look')
+  showBoule(): string {
+    return 'OO';
+  }
+  @Get('isAvailable')
+  isAvailable(): string {
+    return 'Always!';
+  }
+  @Get()
+  mainPageOfJohnBoule(): string {
+    return 'Hello ! Welcome to the "Boule a John" main page. You can : /look to see it or /isAvailable to ask if it is available';
+  }
+}
