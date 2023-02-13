d15f1eaac756299ae865ac19a2ca5294100c8f71
Nicolas Jaros <njaros@z4r2p1.42lyon.fr>
2023-02-13 18:05:41 +0100

    event

--- front/react_project/src/main3.tsx
+++ front/react_project/src/main3.tsx
@@ -10,6 +10,10 @@ interface Switch {
   isToggleOn: boolean;
 };
 
+interface TIS_Number {
+  number: number;
+}
+
 class Toggle extends React.Component<{}, Switch> {
   constructor(props: {}) {
     super(props);
@@ -32,6 +36,38 @@ class Toggle extends React.Component<{}, Switch> {
   }
 }
 
+class Toggle2 extends React.Component<{}, TIS_Number> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {number: 0};
+    this.handleClick = this.handleClick.bind(this);
+    this.handleDoubleClick = this.handleDoubleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      number: state.number + 1
+    }));
+  }
+
+  handleDoubleClick() {
+    this.setState(state => ({
+      number: 0
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick} onDoubleClick={this.handleDoubleClick} onMouseLeave={this.handleDoubleClick}>
+        my count is {this.state.number}
+      </button>
+    )  
+}
+}
+
 event.render(
-  <Toggle />
+  <div>
+    <Toggle />
+    <Toggle2 />
+  </div>
 )
