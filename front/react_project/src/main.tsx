2fee7549839883ffb55a7dda1ed17c889ebe27dc
jajalecapouet <nicolas.jarose@gmail.com>
2023-02-13 07:58:40 +0100

    avancee tuto react

--- front/react_project/src/main.tsx
+++ front/react_project/src/main.tsx
@@ -2,21 +2,68 @@ import React from 'react'
 import { useState, useEffect, } from 'react'
 import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
-//import App from './App'
-//import './index.css'
 
-interface Txt {
-  content: string;
-  grade: number;
-}
-
-function composeTxt(txt: Txt) {
-  return txt.content + ' has grade ' + txt.grade;
-}
-
-const txt: Txt = {content: 'dd', grade: 8};
-
-const element = <h1>blbl, {composeTxt(txt)}</h1>
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(element);
+
+interface Img {
+  url: string,
+  alt: string
+}
+
+interface User {
+  name: string,
+  date: Date,
+  img: Img
+};
+
+const michel: User = {
+  name: "Michel",
+  date: new Date(),
+  img: {
+    url: "NaN",
+    alt: "a fish"
+  }
+};
+
+function formatDate(date: Date) {
+  return (
+    <h2>{date.toLocaleDateString()}</h2>
+  )
+}
+
+function Avatar(props: Img) {
+  return (
+    <img className="Avatar"
+      src={props.url}
+      alt={props.alt}
+    />
+  );
+}
+
+function UserInfo(props: Img) {
+  return (
+    <div className="UserInfo">
+      <Avatar url={props.url} alt={props.alt}/>
+      <div className="UserInfo-name">
+        {props.alt}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props: User) {
+  return (
+    <div className="Comment">
+      <UserInfo url={props.img.url} alt={props.img.alt} />
+      <div className='Comment-text'>
+        {props.name}
+      </div>
+      <div className='Comment-date'>
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+root.render(<Comment name={michel.name} date={michel.date} img={michel.img} />);
