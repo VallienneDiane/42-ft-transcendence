5127f3bfed6a3c96112d786dabd2d95d0573828b
Clemence Cartet <ccartet@z4r2p3.42lyon.fr>
2023-02-20 16:59:50 +0100

    transformation fonctions en classes...

---
+++ front/react_project/src/page_modules/ChatModule.tsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import socketIOClient from 'socket.io-client';
+
+const socket = socketIOClient("127.0.0.1:3000", {transports : ['websocket']})
+
+interface Message {
+    text: string;
+    senderName: string;
+}
+
+interface ChatHistory {
+    messages: Message[];
+}
+
+type MyState = {
+    chatHistory: ChatHistory;
+    message: Message;
+} 
+
+function ListMessage(value: Message): JSX.Element {
+    return (
+        <li>
+            <div>{value.senderName}</div>
+            <div>{value.text}</div>
+        </li>
+    )   
+}
+
+class MessageList extends React.Component<ChatHistory> {
+    render() {
+        const listItems: JSX.Element[] = this.props.messages.map(
+            (message, id) => <ListMessage key={id} text={message.text} senderName={message.senderName} />
+        );
+        return (
+            <ul className="messageList">
+                {listItems}
+            </ul>
+        );
+    }
+}
+
+function Title() {
+    return (
+        <div>
+            <h1>Channel 1</h1>
+        </div>
+    )
+}
+
+class SendMessageForm extends React.Component<Message> {
+    constructor(props: Message) {
+        super(props);
+        this.handleMessage = this.handleMessage.bind(this);
+    }
+
+    handleMessage(event: React.ChangeEvent<HTMLInputElement>) {
+        this.props.handle(event.target.value);
+    }
+
+    sendMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        socket.emit('message', message);
+        console.log(this.state,text);
+    }
+
+    render() {
+        const text = this.props.text;
+        const senderName = this.props.senderName;
+        return (
+            <div>
+                <form onSubmit={this.sendMessage}>
+                    <label>
+                        Type your message :  
+                        <input type="text" value={text} onChange={this.handleMessage} />
+                    </label>
+                    <input type="submit" value="send" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default class ChatModule extends React.Component<{}, MyState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = { 
+            chatHistory: { messages: []}, 
+            message: { text: '', senderName: ''} 
+        };
+    }
+
+    handleMessageChange(text: string) {
+        this.setState({message: {text, senderName: 'blop'}});
+    }
+
+    render() {
+        return (
+            <div className="chat">
+                <Title />
+                <MessageList messages={this.state.chatHistory.messages} />
+                <SendMessageForm text={this.state.message.text} senderName={this.state.message.senderName} />
+            </div>
+        )
+    }
+}
