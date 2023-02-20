60d8e9ce99acd8b266598081abe50b3eab679a08
VallienneDiane <diane.vallienne@outlook.com>
2023-02-20 17:09:45 +0100

    try to do jwt token

--- back/nest_project/src/auth/auth.module.ts
+++ back/nest_project/src/auth/auth.module.ts
@@ -1,29 +1,25 @@
 import { Module } from "@nestjs/common";
-import { PassportModule } from "@nestjs/passport";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UserEntity } from "src/user/user.entity";
 import { UserModule } from "src/user/user.module";
-import { UserService } from "src/user/user.service";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from "./auth.jwt.strategy";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./jwt.strategy";
 
+// process.env.SECRET_KEY
 @Module({
-    imports: [
+    imports: 
+    [
         UserModule, 
-        PassportModule.register({
-            defaultStrategy: 'jwt',
-            property: 'user',
-            session: false,
-        }), 
+        PassportModule,
         JwtModule.register({
-            secret: process.env.SECRETKEY, signOptions: {
-                expiresIn: process.env.EXPIRESIN,
-            },
+            secret: process.env.SECRET_KEY,
+            signOptions: { expiresIn: '60s' },
         }),
     ],
     controllers: [AuthController],
-    providers: [AuthService, UserService, JwtStrategy],
+    providers: [AuthService, JwtStrategy],
+    exports: [AuthService]
   })
+
   export class AuthModule { }
--- back/nest_project/src/auth/auth.service.ts
+++ back/nest_project/src/auth/auth.service.ts
@@ -1,16 +1,16 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Res } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+// import { Response } from 'express';
+import { AuthDto } from './auth.dto';
+import { UserDto } from 'src/user/user.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private userService: UserService,
-    private jwtService: JwtService
-  ) {}
+  constructor(private userService: UserService, private jwtService: JwtService) {}
 
-  async validateUserInfos(login: string, password: string) {
+  async validateUser(login: string, password: string): Promise<AuthDto> {
     const user = await this.userService.findByLogin(login);
     if(!user) {
         throw new HttpException('User not found', HttpStatus.UNAUTHORIZED);
@@ -22,11 +22,12 @@ export class AuthService {
     return user;
   }
 
-  async login(user: any)
-  {
-    const payload = {login: user.login, sub: user.id };
+  async loginJwt(user: UserDto) {
+    const payload = { login: user.login, sub: user.id};
+
     return {
       access_token: this.jwtService.sign(payload),
-    }
+    };
   }
+    // response.cookie('access_token', access_token, {httpOnly: true});
 }
