dc4a4d0b05784731c8cc2abac8e79e280b873c1d
amarchal <aleximarchal@hotmail.com>
2023-02-24 17:23:02 +0100

    protectedroutes to fix

--- front/react_project/src/App.tsx
+++ front/react_project/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import SignupForm from './components/SignupForm'
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Profile from './components/Profile';
 import UserProvider from './user/UserProvider';
 import ProtectedRoutes from './components/ProtectedRoutes';
@@ -11,32 +11,34 @@ import Layout from './components/Layout'
 import Game from './components/Game'
 
 function App() {
-  let user = {
-    token: "",
-    id: 666,
-    login: "",
-    email: "",
-    password: "",
-  };
+  // let user = {
+  //   token: "",
+  //   id: 666,
+  //   login: "",
+  //   email: "",
+  //   password: "",
+  // };
 
     return (
       <div className="App">
-        <UserProvider user={user}>
+        {/* <UserProvider user={user}> */}
+        <BrowserRouter >
           <Routes>
             <Route element={<Layout />}>
               <Route path='/login' element={<LoginForm />} />
               <Route path='/signin' element={<SignupForm />} />
               <Route element={<ProtectedRoutes/>}>
+                <Route path='/' element={<Home />} />
                 <Route path='/game' element={<Game />} />
+                <Route path='/chat' element={<Chat />} />
                 <Route path='/profile' element={<Profile />} />
                 <Route path='/profile/:id' element={<Profile />} />
-                <Route path='/' element={<Home />} />
-                <Route path='/chat' element={<Chat />} />
               </Route>
             </Route>
 
           </Routes>
-        </UserProvider>
+        </BrowserRouter>
+        {/* </UserProvider> */}
       </div>
   )
 }
