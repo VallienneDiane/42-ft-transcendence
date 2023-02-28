89f61b6918a7aecf0750446d909f04cc0d8ab7c9
Nicolas Jaros <njaros@z3r3p5.42lyon.fr>
2023-02-28 13:07:51 +0100

    repair of master

--- back/nest_project/src/socket/socketEvents.ts
+++ back/nest_project/src/socket/socketEvents.ts
@@ -5,32 +5,17 @@ import { join } from './chatUtils/channels';
 import { Base64 } from "js-base64";
 import * as jsrsasign from 'jsrsasign';
 
-
-@WebSocketGateway({transports: ['websocket']}) // gateway is listening
-export class PingEvent {
-    @WebSocketServer() // create websocket server
-    server: Server;
-
-    //receive events
-    @SubscribeMessage('ping')
-    handleEvent(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
-        // send an event
-        console.log(client.id + data);
-        this.server.emit('pong', client.id, data);
-    }
-}
-
 @WebSocketGateway({transports: ['websocket']})
 export class Chat implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer()
     server: Server;
 
     handleConnection(client: Socket) {
-        console.log(jsrsasign.KJUR.jws.JWS.parse(client.handshake.auth['token']).payloadObj!.login + ' is connected to chat');
+        console.log(jsrsasign.KJUR.jws.JWS.parse(client.handshake.auth['token']).payloadObj!.login + ' is connected to chat' || "null");
     }
 
     handleDisconnect(client: Socket) {
-        console.log(jsrsasign.KJUR.jws.JWS.parse(client.handshake.auth['token']).payloadObj!.login + ' disconnected');
+        console.log(jsrsasign.KJUR.jws.JWS.parse(client.handshake.auth['token']).payloadObj!.login + ' disconnected' || "null");
     }
 
     @SubscribeMessage('chat')
