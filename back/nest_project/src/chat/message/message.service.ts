8289b2672d40fab223a580d8649f2afa2617d3dd
Clemence Cartet <ccartet@z3r5p5.42lyon.fr>
2023-03-02 11:32:30 +0100

    retreive message private

--- back/nest_project/src/chat/message/message.service.ts
+++ back/nest_project/src/chat/message/message.service.ts
@@ -14,11 +14,24 @@ export class MessageService {
         return this.messagesRepository.save(newMessage);
     }
     // find all messages send to a receiver oreder by date
-    public findByReceiver(roomEntry: string): Promise<MessageEntity[]> {
+    public findByChannel(roomEntry: string): Promise<MessageEntity[]> {
         return this.messagesRepository.find({
             where: {
                 room: roomEntry,
+                isChannel: true,
             },
+            order: {
+                date: "ASC",
+            }
+        })
+    }
+
+    public findByPrivate(personA: string, personB: string): Promise<MessageEntity[]> {
+        return this.messagesRepository.find({
+            where: [
+                { room: personA, sender: personB, isChannel: false },
+                { room: personB, sender: personA, isChannel: false },
+            ],
             order: {
                 date: "ASC",
             }
