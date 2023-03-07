bc610fc08376dd62eafdd4ad04a6ca153c7266c4
root <jonathan.rinna@gmail.com>
2023-03-07 13:06:37 +0100

    initial work on engine main_loop

--- back/nest_project/src/game_engine/game_engine.service.ts
+++ back/nest_project/src/game_engine/game_engine.service.ts
@@ -5,4 +5,9 @@ export class GameEngineService {
 	hello() {
 		console.log('heuuu ca marche ?');
 	}
+
+	main_loop() {
+		console.log("game was updated");
+		setTimeout(this.main_loop, 1000/60);
+	}
 }
