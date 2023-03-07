0d8652c2a2ba18f18a9118c0fb2d740d297a72fc
root <jonathan.rinna@gmail.com>
2023-03-07 10:36:19 +0100

    merge origin

--- front/react_project/src/App.tsx
+++ front/react_project/src/App.tsx
@@ -2,8 +2,8 @@ import './App.css'
 import NavBar from './components/Navbar'
 import PingHandler from './components/SocketHandler'
 import ChatModule from './components/ChatModule';
-import SignupForm from './components/SignupForm'
-import LoginForm from './components/LoginForm'
+import SignupForm from './components/SignupPage'
+import LoginForm from './components/LoginPage'
 import Home from './components/Home'
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Profile from './components/Profile';
@@ -12,6 +12,8 @@ import ProtectedRoutes from './components/ProtectedRoutes';
 import Chat from './components/Chat'
 import Layout from './components/Layout'
 import Game from './components/Game'
+import { io, Socket } from 'socket.io-client';
+
 
 function App() {
   // let user = {
@@ -35,7 +37,6 @@ function App() {
                 <Route path='/game' element={<Game />} />
                 <Route path='/profile' element={<Profile />} />
                 <Route path='/profile/:id' element={<Profile />} />
-                <Route path='/' element={<Home />} />
                 <Route path='/chat' element={<ChatModule />} />
               </Route>
             </Route>
