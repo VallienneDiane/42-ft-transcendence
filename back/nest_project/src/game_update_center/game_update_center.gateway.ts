cbee76c1c858fd6b3f5d1b99afd18cbc1e05d700
amarchal <aleximarchal@hotmail.com>
2023-03-08 10:22:44 +0100

    first merge front back of the game

--- back/nest_project/src/game_update_center/game_update_center.gateway.ts
+++ back/nest_project/src/game_update_center/game_update_center.gateway.ts
@@ -3,14 +3,25 @@ import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from
 import { Server } from 'socket.io';
 import { GameEngineService } from 'src/game_engine/game_engine.service';
 
+interface gameState {
+  ballPosition: {x: number, y: number},
+  paddleOne: {x: number, y: number },
+  paddleTwo: {x: number, y:number }
+}
+
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
 export class GameUpdateCenterGateway implements OnModuleInit{
-
 	constructor(private readonly gameEngineService: GameEngineService) {}
+
+  private gamestate: gameState = {
+    ballPosition: {x: 0.5, y: 0.5},
+    paddleOne: {x: 1, y: 0.5 },
+    paddleTwo: {x: 0, y: 0.5 }
+  }
 
   @WebSocketServer()
   server: Server;
@@ -19,15 +30,20 @@ export class GameUpdateCenterGateway implements OnModuleInit{
     const io = require('socket.io')(this.server);
     console.log('test');
     this.server.on('connection', (socket) => {
-      console.log(socket.id);
-      console.log('Connected');
+      console.log("socket ID", socket.id, 'Is connected');
+      // console.log('Connected');
     })
   }
 
   @SubscribeMessage('Game_Input')
-  OnGame_Input(@MessageBody() body: any) {
-    console.log(body);
+  OnGame_Input(@MessageBody() body: string) {
+    if (body === 'ArrowUp') {
+      this.gamestate.paddleOne.y > 0.08 ? this.gamestate.paddleOne.y -= 0.02 : null;
+    }
+    else if (body === 'ArrowDown') {
+      this.gamestate.paddleOne.y < 0.92 ? this.gamestate.paddleOne.y += 0.02 : null;
+    }
     this.gameEngineService.hello();
-    this.server.emit('Game_Update', 'game update received')
+    this.server.emit('Game_Update', this.gamestate)
   }
 }
