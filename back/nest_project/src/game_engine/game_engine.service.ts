ba15b4b5d7d46aaacc4e85df4c11e3c2de4bc588
root <jonathan.rinna@gmail.com>
2023-03-08 12:42:43 +0100

    some progress on input, just before testing

--- back/nest_project/src/game_engine/game_engine.service.ts
+++ back/nest_project/src/game_engine/game_engine.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { Ball } from './Ball';
+import { Vec2 } from './math/Vec2';
+
+interface gameState {
+	ballPosition: {x: number, y: number},
+	paddleOne: {x: number, y: number },
+	paddleTwo: {x: number, y:number }
+  }
 
 @Injectable()
 export class GameEngineService {
+	ball: Ball = new Ball(0.5, 0.5, 0.1, new Vec2(0, 0));
 
-	ball: Ball;
+	public gs: gameState;
+
+	constructor() {
+		this.gs = {
+			ballPosition: {x: 0.5, y: 0.5},
+			paddleOne: {x: 1, y: 0.5},
+			paddleTwo: {x: 0, y: 0.5}
+		};
+	}
 
 	hello() {
 		console.log('heuuu ca marche ?');
@@ -12,6 +28,8 @@ export class GameEngineService {
 
 	main_loop() {
 		this.ball.update_self_position();
+		this.gs.ballPosition.x = this.ball.position.x;
+		this.gs.ballPosition.y = this.ball.position.y;
 		console.log("game was updated");
 	}
 
