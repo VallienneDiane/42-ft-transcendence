66dfd8a2d7269b79e177443db2077f1ea4caf1e3
Nicolas Jaros <njaros@z3r4p3.42lyon.fr>
2023-03-10 16:45:22 +0100

    pouet

--- front/react_project/src/components/ProtectedRoutes.tsx
+++ front/react_project/src/components/ProtectedRoutes.tsx
@@ -1,26 +1,25 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { accountService } from "../services/account.service";
 import SocketContext from './context';
-import React from "react";
+import React, { useEffect } from "react";
 
 const ProtectedRoutes = () => {
     const location = useLocation();
     const {socket, createSocket, disconnect} = React.useContext(SocketContext);
     const token = accountService.getToken();
    
-    if (token !== null)
-    {
-        if (token !== socket.auth.token as string)
-        {
+    useEffect(() => {
+        if (token !== null) {
+            if (token !== socket.auth.token as string) {
             console.log("ne doit creer qu'une socket");
             disconnect();
             createSocket();
+            }
         }
-    }
-    socket.on("test", () => {
-        console.log("Id", socket.id);
-      });
-
+        socket.on("test", () => {
+            console.log("Id", socket.id);
+        });
+    })
     return accountService.isLogged() ? <Outlet/> : <Navigate to="/login"/>;
 }
 
