69fdb4dee2be9d319fecb5c11630e701950bc17b
jaja_MintDebian <nicolas.jarose@gmai.com>
2023-03-15 11:29:51 +0100

    tentative de reglage de dependances de modules nest

--- back/nest_project/src/chat/channel/channel.module.ts
+++ back/nest_project/src/chat/channel/channel.module.ts
@@ -1,10 +1,16 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { linkUCModule } from "../link_users_channels/linkUC.module";
+import { LinkUCService } from "../link_users_channels/linkUC.service";
+import { MessageModule } from "../message/message.module";
+import { MessageService } from "../message/message.service";
 import { ChannelEntity } from "./channel.entity";
 import { ChannelService } from "./channel.service";
 
 @Module({
 	imports: [
+		forwardRef(() => linkUCModule),
+		forwardRef(() => MessageModule),
 		TypeOrmModule.forFeature([ChannelEntity])
 	],
 	providers: [ChannelService],
--- back/nest_project/src/chat/link_users_channels/linkUC.module.ts
+++ back/nest_project/src/chat/link_users_channels/linkUC.module.ts
@@ -1,10 +1,16 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ChannelModule } from "../channel/channel.module";
+import { ChannelService } from "../channel/channel.service";
+import { MessageModule } from "../message/message.module";
+import { MessageService } from "../message/message.service";
 import { LinkUCEntity } from "./linkUC.entity";
 import { LinkUCService } from "./linkUC.service";
 
 @Module({
 	imports: [
+		forwardRef(() => ChannelModule),
+		forwardRef(() => MessageModule),
 		TypeOrmModule.forFeature([LinkUCEntity])
 	],
 	providers: [LinkUCService],
