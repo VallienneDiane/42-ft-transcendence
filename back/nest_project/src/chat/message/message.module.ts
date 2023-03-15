13026524c9478bc402470e19445fcef07632b73b
jaja_MintDebian <nicolas.jarose@gmai.com>
2023-03-15 11:29:51 +0100

    tentative de reglage de dependances de modules nest

--- back/nest_project/src/chat/message/message.module.ts
+++ back/nest_project/src/chat/message/message.module.ts
@@ -1,10 +1,16 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ChannelModule } from "../channel/channel.module";
+import { ChannelService } from "../channel/channel.service";
+import { linkUCModule } from "../link_users_channels/linkUC.module";
+import { LinkUCService } from "../link_users_channels/linkUC.service";
 import { MessageEntity } from "./message.entity";
 import { MessageService } from "./message.service";
 
 @Module({
     imports: [
+        forwardRef(() => ChannelModule),
+        forwardRef(() => linkUCModule),
         TypeOrmModule.forFeature([MessageEntity])
     ],
     providers: [MessageService],
