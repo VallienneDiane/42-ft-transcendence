42989fc63cb981bbda813bc10d3d6e0060bf5526
ClemenceCartet <bloumin6@gmail.com>
2023-03-30 16:03:59 +0200

    settings channel ok

--- front/react_project/src/components/ChatModule/ChatSidebar.tsx
+++ front/react_project/src/components/ChatModule/ChatSidebar.tsx
@@ -1,14 +1,76 @@
-import { useState, useContext, useEffect, useRef } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import SocketContext from "../context";
+import { useForm } from 'react-hook-form';
 import { accountService } from "../../services/account.service";
-import { IDest } from "../../models";
 import { JwtPayload } from "jsonwebtoken";
+import { IDest, IChannel } from "../../models";
+
+function ModifyChannel(props: {channel: IChannel}) {
+    const {socket} = useContext(SocketContext);
+    const { register, formState: { errors }, handleSubmit } = useForm<IChannel>({ 
+        defaultValues: { 
+        name: props.channel.name,
+        password: props.channel.password,
+        channelPass: props.channel.channelPass,
+        inviteOnly: props.channel.inviteOnly,
+        persistant: props.channel.persistant,
+        onlyOpCanTalk: props.channel.onlyOpCanTalk, 
+        hidden: props.channel.hidden } 
+    });
+    const [showChannelPass, setShowChannelPass] = useState<boolean>(false);
+
+    const changeState = (event: any) => {
+        setShowChannelPass(event.target.checked);
+    }
+
+    const onSubmit = (data: IChannel) => {
+        console.log(data)
+        socket.emit('modifyChannel', {
+            name: data.name,
+            password: data.password,
+            channelPass: data.channelPass,
+            inviteOnly: data.inviteOnly,
+            persistant: data.persistant,
+            onlyOpCanTalk: data.onlyOpCanTalk,
+            hidden: data.hidden,
+        });
+    };
+
+    return (
+        <form className="settingList">
+            <li>Name 
+                <input className="inputChannel" {...register("name", { required: true, minLength: 2, maxLength: 20, pattern: /^[A-Za-z0-9]+$/i })}
+                    type="text"
+                    placeholder=""
+                />
+                {errors.name && <div className="logError">Channel name is required</div>}
+            </li>
+            <li>Password
+                <input type="checkbox" {...register("password")} onChange={changeState}/>
+            </li>
+            {showChannelPass && (
+                <li>
+                    <input className="inputChannel" {...register("channelPass", { required: true, minLength: 2, maxLength: 20, pattern: /^[A-Za-z0-9]+$/i })}
+                        type="password"
+                        placeholder=""
+                    /> 
+                </li>)}
+            {showChannelPass && errors.channelPass && <div className="logError">Your password is not valid</div>}
+            <li>Invite Only<input type="checkbox" {...register("inviteOnly")}/></li>
+            <li>Persistant<input type="checkbox" {...register("persistant")}/></li>
+            <li>Only OP Can Talk<input type="checkbox" {...register("onlyOpCanTalk")}/></li>
+            <li>Hidden<input type="checkbox" {...register("hidden")}/></li>
+            <li><button type="submit">Save</button></li>
+        </form>
+    )
+}
 
 export function SidebarChannel(props: {dest: IDest, handleClose: any}) {
     const {socket} = useContext(SocketContext);
     const ref = useRef<HTMLDivElement>(null);
     const [members, setMembers] = useState<string[]>([]);
     const [onClickMembers, setOnClickMembers] = useState<boolean>(false);
+    const [onClickSettings, setOnClickSettings] = useState<boolean>(false);
     const me: JwtPayload = accountService.readPayload()!;
 
     const leaveChannel = () => {
@@ -23,6 +85,10 @@ export function SidebarChannel(props: {dest: IDest, handleClose: any}) {
 
     const listMembers = () => {
         setOnClickMembers((onClickMembers) => !onClickMembers)
+    }
+
+    const showSettings = () => {
+        setOnClickSettings((onClickSettings) => !onClickSettings)
     }
 
     const showUserParam = () => {
@@ -60,7 +126,7 @@ export function SidebarChannel(props: {dest: IDest, handleClose: any}) {
                             if (member !== me.login)
                                 return (<li key={id} onClick={showUserParam}>{member}</li>)
                             else
-                                return (<li key={id}>{member}</li>)
+                                return (<li key={id}>{member}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M288 192h17.1c22.1 38.3 63.5 64 110.9 64c11 0 21.8-1.4 32-4v4 32V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V339.2L248 448h56c17.7 0 32 14.3 32 32s-14.3 32-32 32H160c-53 0-96-43-96-96V192.5c0-16.1-12-29.8-28-31.8l-7.9-1C10.5 157.6-1.9 141.6 .2 124s18.2-30 35.7-27.8l7.9 1c48 6 84.1 46.8 84.1 95.3v85.3c34.4-51.7 93.2-85.8 160-85.8zm160 26.5v0c-10 3.5-20.8 5.5-32 5.5c-28.4 0-54-12.4-71.6-32h0c-3.7-4.1-7-8.5-9.9-13.2C325.3 164 320 146.6 320 128v0V32 12 10.7C320 4.8 324.7 .1 330.6 0h.2c3.3 0 6.4 1.6 8.4 4.2l0 .1L352 21.3l27.2 36.3L384 64h64l4.8-6.4L480 21.3 492.8 4.3l0-.1c2-2.6 5.1-4.2 8.4-4.2h.2C507.3 .1 512 4.8 512 10.7V12 32v96c0 17.3-4.6 33.6-12.6 47.6c-11.3 19.8-29.6 35.2-51.4 42.9zM400 128a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zm48 16a16 16 0 1 0 0-32 16 16 0 1 0 0 32z"/></svg></li>)
                         }
                         )}
                     </ul>
@@ -69,7 +135,12 @@ export function SidebarChannel(props: {dest: IDest, handleClose: any}) {
                     <li onClick={inviteUser}>Invite</li>
                 ) : null }
                 { props.dest.isOp ? (
-                    <li>Settings</li>
+                    <React.Fragment>
+                        <li onClick={showSettings}>Settings</li>
+                        {onClickSettings && (
+                           <ModifyChannel channel={props.dest.channel!}/>
+                        )}
+                    </React.Fragment>
                 ) : null }
                 <li onClick={leaveChannel}>Leave</li>
             </ul>
