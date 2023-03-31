940d033645c72228db671aed09a9f13b4e37899c
Nicolas Jaros <njaros@z4r3p3.42lyon.fr>
2023-03-31 14:00:07 +0200

    compilation back ok

--- back/nest_project/src/chat/channel/channel.module.ts
+++ back/nest_project/src/chat/channel/channel.module.ts
@@ -1,9 +1,5 @@
-import { forwardRef, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { linkUCModule } from "../link_users_channels/linkUC.module";
-import { LinkUCService } from "../link_users_channels/linkUC.service";
-import { MessageModule } from "../message/message.module";
-import { MessageService } from "../message/message.service";
 import { ChannelEntity } from "./channel.entity";
 import { ChannelService } from "./channel.service";
 
--- back/nest_project/src/chat/link_users_channels/linkUC.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsBoolean, IsNotEmpty, IsNumber, IsString } from "class-validator";
-
-export class linkUCDto {
-	readonly id: string;
-	@IsNotEmpty() @IsString() readonly userId: string;
-	@IsNotEmpty() @IsString() readonly channelId: string;
-	readonly date: Date;
-	@IsNotEmpty() @IsBoolean() readonly isOp: boolean;
-}
