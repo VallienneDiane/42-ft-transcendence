b799951931979bc76fabb252a16c5365c96154a5
ClemenceCartet <bloumin6@gmail.com>
2023-04-03 17:01:38 +0200

    changeLoc

--- front/react_project/src/components/ChatModule/ChatSidebar.tsx
+++ front/react_project/src/components/ChatModule/ChatSidebar.tsx
@@ -163,7 +163,7 @@ export function SidebarUser(props: {handleClose: any, dest: IDest}) {
 
     const kickUser = () => {
         // search
-        socket.emit('kickUser', "nami", props.dest.Loc);
+        socket.emit('kickUser', "nami", props.dest.loc);
     }
 
     useEffect(() => {
@@ -213,7 +213,7 @@ export function Header(props: {dest: IDest}) {
             : <svg className="iconChannels" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg> }
                 {props.dest.Loc}
             </h1>
-            {props.dest.Loc !== "general" && <button onClick={onClickSidebar}><svg className="iconChannels" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><path d="M56 472a56 56 0 1 1 0-112 56 56 0 1 1 0 112zm0-160a56 56 0 1 1 0-112 56 56 0 1 1 0 112zM0 96a56 56 0 1 1 112 0A56 56 0 1 1 0 96z"/></svg></button>}
+            {props.dest.loc !== "general" && <button onClick={onClickSidebar}><svg className="iconChannels" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><path d="M56 472a56 56 0 1 1 0-112 56 56 0 1 1 0 112zm0-160a56 56 0 1 1 0-112 56 56 0 1 1 0 112zM0 96a56 56 0 1 1 112 0A56 56 0 1 1 0 96z"/></svg></button>}
         </div>
     )
 }
