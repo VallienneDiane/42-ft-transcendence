41314e65f59f57b95754208bf3851a59ddef841b
ClemenceCartet <bloumin6@gmail.com>
2023-04-07 14:03:51 +0200

    suppression node-modules, icon ...

--- back/nest_project/src/user/relation/friend/friend.module.ts
+++ back/nest_project/src/user/relation/friend/friend.module.ts
@@ -4,9 +4,10 @@ import { FriendEntity } from "./friend.entity";
 import { FriendService } from "./friend.service";
 import { FriendController } from "./friend.controller";
 import { JwtStrategy } from "src/auth_strategies/jwt.strategy";
+import { UserModule } from "src/user/user.module";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([FriendEntity])],
+	imports: [TypeOrmModule.forFeature([FriendEntity]), UserModule],
 	controllers: [FriendController],
 	providers: [FriendService, JwtStrategy], //  seulement ces fonctions ont accès à la table Friend
 	exports: [FriendService],
