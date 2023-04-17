3ec902dcfdd8c8c3781f29f6a48c8c6ff1179ff4
amarchal <aleximarchal@hotmail.com>
2023-04-17 16:43:20 +0200

    spect mode in progress

--- front/react_project/src/components/MatchsInProgress.tsx
+++ front/react_project/src/components/MatchsInProgress.tsx
@@ -30,9 +30,9 @@ interface inProgressProps {
 const MatchsInProgress: React.FC<inProgressProps> = (props) => {
 
     const [matchs, setMatchs] = useState<MatchState[]>([
-        // {player1_login: "JOUEUR1", player2_login: "JOUEUR2", player1_score: 3, player2_score: 1, super_game_mode: false, game_has_started: true},
-        // {player1_login: "Rorger", player2_login: "Conrnard", player1_score: 2, player2_score: 0, super_game_mode: false, game_has_started: true},
-        // {player1_login: "Roger", player2_login: "Connard", player1_score: 2, player2_score: 0, super_game_mode: false, game_has_started: true},
+        {player1_login: "JOUEUR1", player2_login: "JOUEUR2", player1_score: 3, player2_score: 1, super_game_mode: false, game_has_started: true},
+        {player1_login: "Rorger", player2_login: "Conrnard", player1_score: 2, player2_score: 0, super_game_mode: false, game_has_started: true},
+        {player1_login: "Roger", player2_login: "Connard", player1_score: 2, player2_score: 0, super_game_mode: false, game_has_started: true},
         // {player1_login: "Michellangelloooooooooooooooooooooooooooooooooooooiiiiii", player2_login: "Michellangelloooooooooooooooooooooooooooooooooooooiiiiiifez", player1_score: 0, player2_score: 10, super_game_mode: false, game_has_started: true},
         // {player1_login: "Michellangelloooooooooooiiiiii", player2_login: "Oui", player1_score: 0, player2_score: 10, super_game_mode: false, game_has_started: true},
         // {player1_login: "Michellangeiiii", player2_login: "Ouiii", player1_score: 0, player2_score: 10, super_game_mode: false, game_has_started: true},
@@ -81,8 +81,9 @@ const MatchsInProgress: React.FC<inProgressProps> = (props) => {
     }, [props.socket]);
 
     const watchMatch = (event: React.MouseEvent<HTMLDivElement>) => {
-        console.log("je veux voir le match de ", event.currentTarget.getAttribute('key'));
-        props.socket.emit('', event.currentTarget.getAttribute('key'))
+        // console.log("event", event.currentTarget.attributes);
+        // console.log("je veux voir le match de ", event.currentTarget.getAttribute('data-key'));
+        props.socket.emit('Spectator_Request', event.currentTarget.getAttribute('data-key'))
     }
 
     return (
@@ -100,7 +101,7 @@ const MatchsInProgress: React.FC<inProgressProps> = (props) => {
                     
                     matchs.map((match: MatchState) => {
                         return (
-                            <div className="match" key={match.player1_login} onClick={watchMatch}>
+                            <div className="match" key={match.player1_login} data-key={match.player1_login} onClick={watchMatch}>
                                 <div><span>{match.player1_login}</span></div>
                                 <div><span>{match.player1_score}</span></div>
                                 <div className="watchMatch">Watch in direct<FontAwesomeIcon icon={faCircle} className="redDot"/></div>
