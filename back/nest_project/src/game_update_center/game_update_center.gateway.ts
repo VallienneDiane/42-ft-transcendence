2b3bff421f733cd434622529a38849a115c4c62a
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-05-12 11:08:43 +0200

    correctio  master and ask invite

--- back/nest_project/src/game_update_center/game_update_center.gateway.ts
+++ back/nest_project/src/game_update_center/game_update_center.gateway.ts
@@ -131,15 +131,20 @@ export class GameUpdateCenterGateway implements OnGatewayInit, OnGatewayConnecti
     // check if undefined is it is then set the value to 0 instead
     nbr_of_socket = nbr_of_socket ?? 0;
     this.login_to_nbr_of_active_socket.set(user_entity.login, ++nbr_of_socket);
-    for (let index = 0; index < this.private_space.length; index++) {
-      const element = this.private_space[index];
-      if (element.target_client_login === this.socketID_UserEntity.get(client.id).login) {
-        this.server.to(client.id).emit("Invitation", {for: element.target_client_login, by: this.socketID_UserEntity.get(element.waiting_client_socket.id).login, send: true, super_game_mode: element.super_game_mode});
-      }
-    }
+    
     //this.transfer_all_match(client);
     console.log("in handle connection nbr_of socket vaut : ", nbr_of_socket);
     this.logger.debug("client Connected---------------- socket id : " + client.id + " client login" + user_entity.login);
+  }
+
+  @SubscribeMessage("Ask_Invitation")
+  handle_resend_invite(@ConnectedSocket() client: Socket) {
+    for (let index = 0; index < this.private_space.length; index++) {
+      const element = this.private_space[index];
+      if (element.target_client_login === this.socketID_UserEntity.get(client.id).login || client === element.waiting_client_socket) {
+        this.server.to(client.id).emit("Invitation", {for: element.target_client_login, by: this.socketID_UserEntity.get(element.waiting_client_socket.id).login, send: true, super_game_mode: element.super_game_mode});
+      }
+    }
   }
 
   @SubscribeMessage("Get_Matches")
@@ -626,6 +631,15 @@ export class GameUpdateCenterGateway implements OnGatewayInit, OnGatewayConnecti
       if (nbr_of_socket <= 1) {
         this.waiting_on_match.delete(this.socketID_UserEntity.get(client.id).login);
         this.logger.debug("client was removed from waiting on game due to disconnection");
+        for (let index = 0; index < this.private_space.length; index++) {
+          const element = this.private_space[index];
+          if (element.target_client_login === this.socketID_UserEntity.get(client.id).login) {
+            this.server.to(element.waiting_client_socket.id).emit("Invitation", {for: element.target_client_login, by: this.socketID_UserEntity.get(element.waiting_client_socket.id).login, send: true, super_game_mode: element.super_game_mode});
+          }
+          if (element.waiting_client_socket === client) {
+            this.server.to(this.get_socketid_by_login(this.socketID_UserEntity, element.target_client_login)).emit("Invitation", {for: element.target_client_login, by: this.socketID_UserEntity.get(element.waiting_client_socket.id).login, send: true, super_game_mode: element.super_game_mode});
+          }
+        }
       }
       else {
         console.log("LOGICAL ERROR, should never be display, unless we try to remove a user.id from the waiting[]/ongoing match socket[] where he was not");
