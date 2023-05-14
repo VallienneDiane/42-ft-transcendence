1023301fc38c58583c157971aa3fa70658bfeb1c
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-05-14 12:37:00 +0200

    rien

--- back/nest_project/src/pong_engine/Simple_Ball.ts
+++ back/nest_project/src/pong_engine/Simple_Ball.ts
@@ -93,8 +93,8 @@ export class Simple_ball {
             this.alive = false;
             return 1;
         }
-        this.x_speed *=1.01;
-        this.y_speed *=1.01;
+        this.x_speed *=1.0001;
+        this.y_speed *=1.0001;
         return 0;
     }
 }
