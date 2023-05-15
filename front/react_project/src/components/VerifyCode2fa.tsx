f2e85e42b79eede8471f47342de647d499f8134e
VallienneDiane <diane.vallienne@outlook.com>
2023-05-15 13:24:11 +0200

    2fa code verify error handle, css verifycode

--- front/react_project/src/components/VerifyCode2fa.tsx
+++ front/react_project/src/components/VerifyCode2fa.tsx
@@ -1,5 +1,6 @@
 import * as yup from 'yup';
 import "../styles/VerifyCode2fa.scss"
+import React, { useState } from 'react';
 import { VerifyCodeForm } from "../models";
 import { useForm } from "react-hook-form";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -18,11 +19,13 @@ const VerifyCode2fa:React.FC = () => {
     const location = useLocation();
     const id = location.state?.id;
     let navigate = useNavigate();
+    const [errorCode, setErrorCode] = useState<boolean>(false);
     const {register, handleSubmit, formState: {errors}} = useForm<VerifyCodeForm>({
         resolver: yupResolver(schema)
     });
-
+    
     const verifySubmittedCode = (data: VerifyCodeForm) => {
+        data.id = id;
         schema.validate(data);
         accountService.verifyCode2fa(data)
         .then(response => {
@@ -37,7 +40,10 @@ const VerifyCode2fa:React.FC = () => {
                 });
             }
         })
-        .catch(error => console.log(error));
+        .catch(error => {
+            setErrorCode(true);
+            console.log(error);
+        });
     }
     
 return (
@@ -47,6 +53,7 @@ return (
             <h2>Check your Google Authentificator application</h2>
                 <input type="text" {...register("code")} name="code" placeholder="Enter the code"/>
                 {errors.code && <p className="errors">{errors.code.message}</p>}
+                {errorCode ? <p className="errors">Wrong code entered</p> : null}
                 <button type="submit">Submit</button>
             </form>
         </div>
